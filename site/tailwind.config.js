cb20d8d8a7a491f74a7c5d5bc7e688cba6b9e456
Colin Calvert <github@whoisc.cc>
2020-10-31 17:43:50 -0500

    [added] Initial template layout [added] Initial support for color config [added] Support for Tailwind CSS

---
+++ site/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: 'var(--color-primary)',
+      secondary: 'var(--color-secondary)',
+      'accent-1': 'var(--color-accent-1)',
+      'accent-2': 'var(--color-accent-2)',
+      'accent-3': 'var(--color-accent-3)'
+    }
+  }
+}
